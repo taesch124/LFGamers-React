7dd879e0b57f7c19fce95fdf91573d695f2d2b2e
Saranjeet Singh <saranjeet.mca@gmail.com>
2019-03-06 18:30:11 -0500

    css and sticky

--- src/components/game-components/GamePanel.js
+++ src/components/game-components/GamePanel.js
@@ -9,7 +9,7 @@ function GamePanel(props) {
                 <img className="game-poster" alt={game.name} src={game.cover ? `https:${game.cover.url}` : 'https://via.placeholder.com/180'} />
             </div>
             <div className="card-content">
-                <span className="right" onClick={(e) => {e.stopPropagation(); props.onGameFavorite(game._id);}}>Favorite</span>
+                <span className="right" title="ADD TO FAVORITES" onClick={(e) => {e.stopPropagation(); props.onGameFavorite(game._id);}}>♥</span>
                 <h4>{game.name}</h4>
                 <p className="left-align">Rating: {game.rating ? game.rating.toFixed(1) : "N/A"}</p>
                 <p className="left-align">Popularity: {game.popularity ? game.popularity.toFixed(0) : "N/A"}</p>
